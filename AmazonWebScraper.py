16491378bfd1fe206fc802d8f2d60d86120aed71
ada516 <jan.johannsen@haw-hamburg.de>
2022-10-09 17:12:38 +0200

    mail change

--- AmazonWebScraper.py
+++ AmazonWebScraper.py
@@ -93,8 +93,7 @@ def notify(changeditems):
     em['To'] = email_reciever
     em['subject'] = subject
     em.set_content(emailText)
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+    with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
         smtp.login(email_sender,email_pw)
         smtp.sendmail(email_sender,email_reciever,em.as_string())
 
